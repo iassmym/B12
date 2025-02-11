dcf647905171035be52b207ecd929afc1bcc45c7
Iasmym <iasmympatricia7@gmail.com>
2025-02-11 19:39:00 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,61 @@
+// // var listanome = ["iasmym", "beatriz","luisa"]
+
+
+
+
+
+// // for(var index = 0; index < listanome.length; index++){
+// //     document.write(`incrivel divas ${listanome[index]} <br>`)
+
+// // }
+
+// // // console.log(listanome[0])
+// // // console.log(listanome[1])
+// // // console.log(listanome[2])
+// // document.write(`incrivel ${listanome[1]}`)
+
+
+// var listaCompras = [];
+
+// var produto = prompt("digite o nome do produto para adicionar na lista")
+
+// listaCompras.push(produto);
+
+// for(var i = 0; i < 4; i++) {
+//     var produto = prompt("digite o nome do produto para adicioar na lista");
+//     listaCompras.push(produto);
+// }
+// function exibirLista(){
+//    console.log(listaCompras)
+//    for(var i = 0; i < listaCompras.length; i++) {
+//     document.write(`<li>${listaCompras[i]}</li>`)
+//    }
+// }
+  
+
+// exibirLista();
+
+// // um sistema que crie uma lista de tarefas
+// // peça ao usuario que digite as suas tarefas e insira elas 
+// // na lista criada ao final do sistema
+//     var listaTarefa =[]
+
+//     for(var i = 0; i < 4; i++) {
+//         var tarefa = prompt("digite o nome do produto para adicioar na lista");
+//         listaTarefa.push(tarefa);
+//     }
+//     function
+
+// var listaFilmes =[]
+
+// for( var i = 0; i < 3; i ++){
+//     var filmes =  prompt ("digite um filme:")
+//     listaFilmes.push(filmes)
+
+// }
+
+// function exibirlistaFilmes(){
+//     for(var i = 0; i < listaFilmes.length; i ++)
+//         document.write(`<li> ${listaFilmes[i]} </li>`)
+// }
+// exibirlistaFilmes();
