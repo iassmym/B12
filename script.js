925bfb76ecb61cbc38ae978b5a9d4e94ca292d0a
Iasmym <iasmympatricia7@gmail.com>
2025-02-11 19:41:06 -0300

    copia

--- script.js
+++ script.js
@@ -59,3 +59,76 @@
 //         document.write(`<li> ${listaFilmes[i]} </li>`)
 // }
 // exibirlistaFilmes();
+
+
+
+
+var listaProdutos = [
+    { img: "smartv.png", titulo: "tv", descricao: "tv 50 polegas, samsung smartv luz de led", preco: 2000 },
+    { img: "smartv.png", titulo: "tv led", descricao: "tv 40 polegas, samsung smartv luz de led", preco: 1500 },
+    { img: "smartv.png", titulo: "tv sobre", descricao: "tv 20 polegas, samsung smartv luz de led", preco: 1000 }
+]
+    for (var i = 0; i < listaProdutos.length; i++) {
+        // estou criando um item no html para CADA ELEMENTO/POSIÇÃO DO ARRAY
+        document.write(`<li>
+            <img src="${listaProdutos[i].img}">    
+            <h1>${listaProdutos[i].titulo}</h1>
+            <p>${listaProdutos[i].descricao} </p>
+            <p style="color: gray; text-decoration: line-through;"> ${listaProdutos[i].preco*2}</p>
+            <p style="color: green"> ${listaProdutos[i].preco}</p>
+            <button style="background-color: green; color: white; border-radius: 5px; padding: 10px">compre agora</button>
+            </li>`)
+    }
+
+exibirLista();
+
+
+// var / funçoes / for / array (elementos compostos) / prompt / if
+function exibirLista() {
+    for (var i = 0; i < listaProdutos.length; i++) {
+        // estou criando um item no html para CADA ELEMENTO/POSIÇÃO DO ARRAY
+        document.write(`<li>
+            <img src="${listaProdutos[i].img}">    
+            <h1>${listaProdutos[i].titulo}</h1>
+            <p>${listaProdutos[i].descricao} </p>
+            <p style="color: gray; text-decoration: line-through;"> ${listaProdutos[i].preco*2}</p>
+            <p style="color: green"> ${listaProdutos[i].preco}</p>
+            <button style="background-color: green; color: white; border-radius: 5px; padding: 10px">compre agora</button>
+            </li>`)
+    }
+}
+exibirLista ();
+
+// todo array
+console.log(listaProdutos)
+// [
+//     {
+//         "img": "smartv.png",
+//         "titulo": "tv",
+//         "descricao": "tv 50 polegas, samsung smartv luz de led",
+//         "preco": 2000
+//     },
+//     {
+//         "img": "smartv.png",
+//         "titulo": "tv led",
+//         "descricao": "tv 40 polegas, samsung smartv luz de led",
+//         "preco": 1500
+//     },
+//     {
+//         "img": "smartv.png",
+//         "titulo": "tv sobre",
+//         "descricao": "tv 20 polegas, samsung smartv luz de led",
+//         "preco": 1000
+//     }
+// ]
+// todo elemento selecionado
+console.log(listaProdutos[0])
+// {
+//     "img": "smartv.png",
+//     "titulo": "tv",
+//     "descricao": "tv 50 polegas, samsung smartv luz de led",
+//     "preco": 2000
+// },
+// apenas uma variavel do elemento selecionado
+console.log(listaProdutos[2].titulo)
+// "tv sobre"
